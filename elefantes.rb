de191d01f374808b3bc20bc542ec678281a99ec9
codeamac02 <codeamac02@gmail.com>
2016-05-09 11:46:12 -0500

    Funcion elefantes

---
+++ elefantes.rb
@@ -0,0 +1,11 @@
+def elefantes 
+  i = 1
+    if i == 1 then print "1 elefante se columpiaba sobre la tela de una araña, como veía que resistía fué a llamar a otro elefante"
+      while i < 99 do puts "#{i} elefantes se columpiaban sobre la tela de una araña, como veían que resistía fueron a llamar a otro elefante"
+  i += 1
+        if i == 99 then print "#{i} elefantes se columpiaban y ¡se rompió la telaraña!"
+    end
+      end
+        end
+end
+elefantes
